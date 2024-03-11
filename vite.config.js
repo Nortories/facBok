6bf959b09996fc000385d075eb366ecb4227c6f5
Josh <sooaemalelagij@gmail.com>
2024-03-10 18:28:23 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        productDetail: resolve(__dirname, "src/product_pages/index.html"),
-        productList: resolve(__dirname, "src/product-list/index.html"),
         login: resolve(__dirname, "src/login/index.html"),
-        orders: resolve(__dirname, "src/orders/index.html"),
+        signUp: resolve(__dirname, "src/signUp/index.html"),
       },
     },
   },
