588192266d5e413a46c225df1a7574da61499ef5
Collin Squires <squirrelly392@gmail.com>
2024-03-28 15:31:49 -0600

    basic html and css for new posts

--- src/js/components/Feed.svelte
+++ src/js/components/Feed.svelte
@@ -92,6 +92,11 @@
 </script>
 
 <div class="feed-container">
+  <form id="new-post" action="" method="post">
+    <label for="new-post-content"><b>So what's up?</b></label>
+    <textarea name="new-post-content" id="new-post-content" required></textarea>
+    <input type="submit" class="new-post-btn" value="Create New Post">
+  </form>
   <!-- <div class="feed-overlay"></div> -->
   {#each feed as post}
     <div class="post-container">
@@ -114,6 +119,37 @@
 <style>
   .feed-container {
     height: 100vh;
+  }
+
+  #new-post {
+    border: 3px solid #0056b3;
+    border-radius: 15px;
+    margin-top: 1.5em;
+    padding: 1.5em;
+    background-color: rgba(136, 132, 132, 0.5);
+  }
+
+  #new-post label {
+    display: block;
+  }
+
+  #new-post-content {
+    border: 1px solid black;
+    border-radius: 15px;
+    padding: 0.5em;
+    margin: 1em auto;
+    width: 100%;
+  }
+
+  .new-post-btn {
+    padding: 1rem 1rem;
+    margin-left: 2em;
+    width: 10em;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
   }
 
   .post-container {
@@ -169,7 +205,7 @@
     cursor: pointer;
   }
 
-  button:hover {
+  button:hover, .new-post-btn:hover {
     background-color: #0056b3;
   }
 
