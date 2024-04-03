2098df734e32f76428367342256176fb408c5a85
Austin Nelson <nel.austintyler.son@gmail.com>
2024-04-02 23:20:56 -0600

    jwt - localStorage

--- src/js/postRendering.mjs
+++ src/js/postRendering.mjs
@@ -7,18 +7,29 @@ export async function fetchPosts(type) {
         const response = await fetch("https://facebok-2q7r.onrender.com/posts/");
         const data = await response.json();
         posts.set(data);
-        console.log("posts fetched");
+        console.log("all posts fetched");
         console.log(data);
     } else if (type == "group") {
         
         const response = await fetch("https://facebok-2q7r.onrender.com/posts/3");
         const data = await response.json();
         posts.set(data);
-        console.log("posts fetched");
+        console.log("fetching group posts");
         console.log(data);
     } else if (type == "mine") {
-        const data = [];
+        const token = localStorage.getItem('token');
+        const options = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+        const response = await fetch("https://facebok-2q7r.onrender.com/posts/post/abc123", options);
+        const data = await response.json();
         posts.set(data);
+        console.log("posts fetched");
+        
+        console.log(data);
         console.log(`fetching my posts`);
         
     }
